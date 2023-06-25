035fb5b9d69ee93e055f9189ff5686b3ccf1749e
Anideep <anideepkalia@gmail.com>
2023-06-25 15:05:15 +0530

    this is first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { JobProvider } from "./contexts/JobContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <AuthProvider>
+    <JobProvider>
+      <Router>
+        <App />
+      </Router>
+    </JobProvider>
+  </AuthProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/services/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvoH0MO-4kdvgQxR5_VydnhbhQo6FYwSc",
+  authDomain: "hirer-50ee8.firebaseapp.com",
+  projectId: "hirer-50ee8",
+  storageBucket: "hirer-50ee8.appspot.com",
+  messagingSenderId: "413829345495",
+  appId: "1:413829345495:web:415106725f12913b584092"
+};
+
+// Initialize Firebase
+export const app = firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export const FieldValue = firebase.firestore.FieldValue;
